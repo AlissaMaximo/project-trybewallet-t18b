4c14d43837ab22f487adee094458ae1661d2e690
AlissaMaximo <alissahiratamaximo@gmail.com>
2022-03-14 18:01:10 -0300

    Requirement 2 complete.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
 import Login from './pages/Login';
+import Wallet from './pages/Wallet';
 
 export default function App() {
   return (
-    <Route exact path="/" component={ (props) => <Login { ...props } /> } />
+    <>
+      <Route exact path="/" component={ (props) => <Login { ...props } /> } />
+      <Route exact path="/carteira" component={ (props) => <Wallet { ...props } /> } />
+    </>
   );
 }
