afd889b32b0d64bd014869d5303f3e55bac98caf
AlissaMaximo <alissahiratamaximo@gmail.com>
2022-03-14 18:43:29 -0300

    First five itens from requirement 4 done.

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -6,15 +6,69 @@ class Wallet extends React.Component {
   render() {
     const { email } = this.props;
     return (
-      <header>
-        <h2>TrybeWallet</h2>
-        <p data-testid="email-field">{ email }</p>
-        <p data-testid="header-currency-field">BRL</p>
-        <p data-testid="total-field">
-          {/* aqui entra a despesa total de vdd na frente. */}
-          0
-        </p>
-      </header>
+      <>
+        <header>
+          <h2>TrybeWallet</h2>
+          <p data-testid="email-field">{ email }</p>
+          <p data-testid="header-currency-field">BRL</p>
+          <p data-testid="total-field">
+            {/* aqui entra a despesa total de vdd na frente. */}
+            0
+          </p>
+        </header>
+        <form>
+          <label htmlFor="input-value">
+            Valor
+            <input
+              type="number"
+              id="input-value"
+              name="value"
+              data-testid="value-input"
+            />
+          </label>
+          <label htmlFor="input-desc">
+            Descrição
+            <input
+              type="text"
+              id="input-desc"
+              name="desc"
+              data-testid="description-input"
+            />
+          </label>
+          <label htmlFor="select-currency">
+            Moeda
+            <select data-testid="currency-input" name="currency" id="select-currency">
+              <option value="valor-temporario">opção temporária</option>
+            </select>
+          </label>
+          <label htmlFor="select-currency">
+            Método de pagamento
+            <select
+              data-testid="method-input"
+              name="payment-method"
+              id="select-payment-method"
+            >
+              <option value="cash">Dinheiro</option>
+              <option value="credit-card">Cartão de crédito</option>
+              <option value="debitCard">Cartão de débito</option>
+            </select>
+          </label>
+          <label htmlFor="select-currency">
+            Categoria
+            <select
+              data-testid="tag-input"
+              name="tag"
+              id="select-tag"
+            >
+              <option value="food">Alimentação</option>
+              <option value="leisure">Lazer</option>
+              <option value="work">Trabalho</option>
+              <option value="transport">Transportação</option>
+              <option value="health">Saúde</option>
+            </select>
+          </label>
+        </form>
+      </>
     );
   }
 }
