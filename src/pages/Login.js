08bbbbf63e75b92f4102bfb78288d551e156c40a
AlissaMaximo <alissahiratamaximo@gmail.com>
2022-03-14 17:04:33 -0300

    Email and password verification done.

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,18 +1,63 @@
 import React from 'react';
 
 class Login extends React.Component {
+  state = {
+    password: '',
+    email: '',
+    isDisabled: true,
+  }
+
+  toggleButton = () => {
+    const { email, password } = this.state;
+    const minPasswordLength = 6;
+
+    this.setState({
+      isDisabled: true,
+    });
+
+    if (this.verifyEmail(email) && password.length >= minPasswordLength) {
+      this.setState({
+        isDisabled: false,
+      });
+    }
+  }
+
+  handleInputChange = ({ target: { value, name } }) => {
+    this.setState({
+      [name]: value,
+    }, this.toggleButton);
+  }
+
+  verifyEmail = (email) => {
+    (String(email).toLowerCase().match(/^[^\s@]+@[^\s@]+\.[^\s@]+$/));
+  };
+
   render() {
+    const { isDisabled } = this.state;
+
     return (
       <form>
         <label label htmlFor="input-email">
           E-mail
-          <input type="email" data-testid="email-input" />
+          <input
+            id="input-email"
+            type="email"
+            data-testid="email-input"
+            name="email"
+            onChange={ this.handleInputChange }
+          />
         </label>
-        <label label htmlFor="input-email">
+        <label label htmlFor="input-password">
           Senha
-          <input type="email" data-testid="password-input" />
+          <input
+            id="input-password"
+            type="email"
+            data-testid="password-input"
+            name="password"
+            onChange={ this.handleInputChange }
+          />
         </label>
-        <button type="submit">Entrar</button>
+        <button type="submit" disabled={ isDisabled }>Entrar</button>
       </form>
     );
   }
