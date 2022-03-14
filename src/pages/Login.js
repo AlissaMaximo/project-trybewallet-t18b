76c0d24dcb8ef32e6e8918adf30a664276857a52
AlissaMaximo <alissahiratamaximo@gmail.com>
2022-03-14 16:45:48 -0300

    Form's structure done.

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,7 +2,19 @@ import React from 'react';
 
 class Login extends React.Component {
   render() {
-    return <div>Login</div>;
+    return (
+      <form>
+        <label label htmlFor="input-email">
+          E-mail
+          <input type="email" data-testid="email-input" />
+        </label>
+        <label label htmlFor="input-email">
+          Senha
+          <input type="email" data-testid="password-input" />
+        </label>
+        <button type="submit">Entrar</button>
+      </form>
+    );
   }
 }
 
